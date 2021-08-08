31e42bb3a24070e223c6fd35781558f12ed49966
prashanth <imprash4u@gmail.com>
2021-08-08 14:26:26 +0530

    sorting and printing the combination

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -141,3 +141,45 @@ do
     triplet_frequency[$combination]=$percentage
     echo "percentage of $combination is ${triplet_frequency[$combination]}%"   
 done
+
+#extracting keys and values
+singlet_combinations=( ${!singlet_frequency[@]} )
+singlet_combination_percentages=( ${singlet_frequency[@]} )
+
+doublet_combinations=( ${!doublet_frequency[@]} )
+doublet_combination_percentages=( ${doublet_frequency[@]} )
+
+triplet_combinations=( ${!triplet_frequency[@]} )
+triplet_combination_percentages=( ${triplet_frequency[@]} )
+
+
+#appending all 3 arrays and assigning into other array
+total_combinations+=( ${singlet_combinations[@]} ${doublet_combinations[@]} ${triplet_combinations[@]} )
+total_combination_percentages+=( ${singlet_combination_percentages[@]} ${doublet_combination_percentages[@]} ${triplet_combination_percentages[@]} )
+
+
+
+echo "---------------------------SORTING---------------------------"
+echo  "sorted singlet percentages:"
+echo "$( printf "%s\n" "${singlet_combination_percentages[@]}" | sort -n ) "
+
+echo  "sorted doublet percentages:"
+echo "$( printf "%s\n" "${doublet_combination_percentages[@]}" | sort -n ) "
+
+echo  "sorted triplet percentages:"
+echo "$( printf "%s\n" "${triplet_combination_percentages[@]}" | sort -n ) "
+
+echo "---------------------------WINNERS---------------------------"
+echo "For Singlet Combination"
+print_winner singlet_combinations singlet_combination_percentages
+echo 
+echo "For Doublet Combination"
+print_winner doublet_combinations doublet_combination_percentages
+echo
+
+echo "For Triplet Combination"
+print_winner triplet_combinations triplet_combination_percentages
+echo
+
+echo "For All Combinations"
+print_winner total_combinations total_combination_percentages
